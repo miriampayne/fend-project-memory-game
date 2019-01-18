e5364de367e55d1c0f27a49142ba03687aeaf35e
Miriam Payne <miriampayne@gmail.com>
2019-01-18 20:20:07 +0000

    Refactor all setup code into various functions

--- js/app.js
+++ js/app.js
@@ -18,80 +18,99 @@ const setup = function() {
   let openedCards = [];
   let matchedCards = [];
 
-  // Create and display cards on page
-  for (let i = 0; i < cards.length; i++) {
-  // Loop through each card and create its HTML
-    const cardElement = document.createElement("li");
-  // cardElement.classList.add("show");
-    cardElement.classList.add("card");
-  // Display the card's symbol
-    const cardType = cards[i];
-    const cardIcon = icons[cardType];
-    cardElement.innerHTML = `<i class="fa fa-${cardIcon}"></i>`;
-    deck.appendChild(cardElement);
-  // Event listener for a card
-    cardElement.addEventListener("click", function() {
+  // Initialize game
+  function init() {
+    // Create cards
+    for (let i = 0; i < cards.length; i++) {
+    // Loop through each card and create its HTML
+      const cardElement = document.createElement("li");
+      cardElement.classList.add("card");
+    // Display the card's symbol
+      const cardType = cards[i];
+      const cardIcon = icons[cardType];
+      cardElement.innerHTML = `<i class="fa fa-${cardIcon}"></i>`;
+      deck.appendChild(cardElement);
 
-      const currentCard = this;
-      const previousCard = openedCards[0];
+    // Add click event to each card
+      click(cardElement);
+    }
+  };
 
-  // Prevent same card being clicked twice
-      if(currentCard === previousCard) {
-        return;
-      }
-  // Existing open card
-        if(openedCards.length === 1) {
+  // Click Event
+  function click(cardElement) {
+    // Event listener for a card
+      cardElement.addEventListener("click", function() {
 
-          cardElement.classList.add("open", "show");
-  // Save opened cards
-          openedCards.push(this);
+        const currentCard = this;
+        const previousCard = openedCards[0];
 
+    // Prevent same card being clicked twice
+        if(currentCard === previousCard) {
+          return;
+        }
+    // Existing open card
+          if(openedCards.length === 1) {
 
-  // Compare 2 opened cards
-          if(currentCard.innerHTML === previousCard.innerHTML) {
-  // Matched - if the cards do match, lock the cards in the open position
-            currentCard.classList.add("match");
-            previousCard.classList.add("match");
-
-            matchedCards.push(currentCard, previousCard);
-
-  // Reset opened cards for when matched
-            openedCards = [];
-
-  // Check if game OVER
-            gameOver();
-
-          } else {
-
-  // Wait 500ms, then action:
-            setTimeout(function() {
-  // No match - if the cards do not match, remove the cards from the list and hide the card's symbol
-              currentCard.classList.remove("open", "show");
-              previousCard.classList.remove("open", "show");
-  // Reset opened cards for when no match
-              openedCards = [];
-            }, 500);
-
-
-
-          }
-
-        } else {
-  // No open cards
-            currentCard.classList.add("open", "show");
-  // Save opened cards
+            cardElement.classList.add("open", "show");
+    // Save opened cards
             openedCards.push(this);
 
-        }
-    });
-  };
-  // Create the winning condition
-  function gameOver() {
-    if(matchedCards.length === (icons.length * 2)) {
-      alert("GAME OVER!");
+
+    // Compare 2 opened cards
+            compare(currentCard, previousCard);
+
+          } else {
+    // No open cards
+              currentCard.classList.add("open", "show");
+    // Save opened cards
+              openedCards.push(this);
+
+          }
+      });
     }
-  }
-};
+
+    // Compare 2 open cards
+    function compare(currentCard, previousCard) {
+      if(currentCard.innerHTML === previousCard.innerHTML) {
+// Matched - if the cards do match, lock the cards in the open position
+        currentCard.classList.add("match");
+        previousCard.classList.add("match");
+
+        matchedCards.push(currentCard, previousCard);
+
+// Reset opened cards for when matched
+        openedCards = [];
+
+// Check if game OVER
+        gameOver();
+
+      } else {
+
+// Wait 500ms, then action:
+        setTimeout(function() {
+// No match - if the cards do not match, remove the cards from the list and hide the card's symbol
+          currentCard.classList.remove("open", "show");
+          previousCard.classList.remove("open", "show");
+// Reset opened cards for when no match
+          openedCards = [];
+        }, 500);
+      }
+    }
+
+
+    // Create the winning condition
+    function gameOver() {
+      if(matchedCards.length === (icons.length * 2)) {
+        alert("GAME OVER!");
+      }
+    }
+    // Start Game for first time
+    init();
+  };
+
+
+
+
 // clear out any cards already in deck element to support refresh button
 
 //  shuffle the list of cards using the provided "shuffle" method below
